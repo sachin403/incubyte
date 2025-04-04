db31625eea5dad0b142dfd2d86b20829a67c83b8
Sachin Sharma <sachin.sharma@intelliswift.com>
2025-04-04 21:23:37 +0530

    step3

--- tdd.rb
+++ tdd.rb
@@ -1,8 +1,18 @@
 
 class StringCalculator
 
+  #step-1, step-2
   def add(numbers)
     return 0 if numbers.empty?
     numbers.split(',').map(&:to_i).sum
   end
+
+#step-3
+  def add(numbers)
+  return 0 if numbers.strip.empty?
+  numbers.split(/,|\n/).map(&:to_i).sum
 end
+
+#step-4
+end
+
--- test_case.rb
+++ test_case.rb
@@ -8,5 +8,19 @@ def test_add
         raise "Fail: Expected 11" unless add("1,4,6") == 11
 
         puts "all test cases pass for step-1 , step-2" 
-        
+
+end
+
+
+# Test case Step-1
+
+def test_add
+
+        raise "Fail: Expected 0" unless add("") == 0
+        raise "Fail: Expected 6" unless add("1\n2,3") == 6
+        raise "Fail: Expected 10" unless add("2,5\n7") == 14
+        raise "Fail: Expected 10" unless add("4,6\n8") == 18
+
+        puts "all test cases pass for step-3" 
+
 end
