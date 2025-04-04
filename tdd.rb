21037c32f0b83c23fcc9bd70a68830d76587d7ed
Sachin Sharma <sachin.sharma@intelliswift.com>
2025-04-04 21:38:26 +0530

    step4

--- tdd.rb
+++ tdd.rb
@@ -14,5 +14,16 @@ class StringCalculator
 end
 
 #step-4
+    def add(numbers)
+     return 0 if numbers.empty?
+     
+     delimiter = /,|\n/
+     if numbers.start_with?("//")
+       delimiter, numbers = numbers[2..].split("\n", 2)
+       delimiter = Regexp.escape(delimiter)
+     end
+
+     numbers.split(/#{delimiter}/).map(&:to_i).sum
+    end
 end
 
--- test_case.rb
+++ test_case.rb
@@ -18,9 +18,22 @@ def test_add
 
         raise "Fail: Expected 0" unless add("") == 0
         raise "Fail: Expected 6" unless add("1\n2,3") == 6
-        raise "Fail: Expected 10" unless add("2,5\n7") == 14
-        raise "Fail: Expected 10" unless add("4,6\n8") == 18
+        raise "Fail: Expected 14" unless add("2,5\n7") == 14
+        raise "Fail: Expected 18" unless add("4,6\n8") == 18
 
         puts "all test cases pass for step-3" 
+
+end
+
+# Test case Step-4
+
+ def test_add
+
+        raise "Fail: Expected 0" unless add("") == 0
+        raise "Fail: Expected 3" unless add("//;\n1;2") == 3
+        raise "Fail: Expected 8" unless add("//|\n3|2|3") == 8
+        raise "Fail: Expected 11" unless add("//;\n5;6") == 11      
+
+        puts "all test cases pass for step-4" 
 
 end
